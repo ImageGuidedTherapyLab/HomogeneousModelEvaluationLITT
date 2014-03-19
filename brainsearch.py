0641c988d8db94d5386c80c24da7ba1127155a03
David Fuentes <fuentesdt@gmail.com>
2014-03-19 15:09:10 -0500

    BUG: off by one error

--- brainsearch.py
+++ brainsearch.py
@@ -1397,6 +1397,9 @@ parser.add_option( "--run_fem","--param_file",
 parser.add_option( "--vis_out", 
                   action="store_true", dest="vis_out", default=False,
                   help="visualise output", metavar="bool")
+parser.add_option( "--accum_history", 
+                  action="store_true", dest="accum_history", default=False,
+                  help="accumulate output", metavar="bool")
 parser.add_option( "--ini", 
                   action="store", dest="config_ini", default=None,
                   help="ini FILE containing setup info", metavar="FILE")
@@ -1444,6 +1447,104 @@ if (options.param_file != None):
     fileHandle = file(sys.argv[3],'w')
     fileHandle.write('%f\n' % objfunction )
     fileHandle.flush(); fileHandle.close();
+
+# find the best point for each run
+elif (options.accum_history != None):
+  resultfileList = [
+  './workdir/Study0035/0530/',
+  './workdir/Study0030/0495/',
+  './workdir/Study0023/0433/',
+  './workdir/Study0030/0497/',
+  './workdir/Study0030/0491/',
+  './workdir/Study0030/0496/',
+  './workdir/Study0030/0490/',
+  './workdir/Study0017/0378/',
+  './workdir/Study0025/0438/',
+  './workdir/Study0025/0435/',
+  './workdir/Study0025/0440/',
+  './workdir/Study0025/0436/',
+  './workdir/Study0028/0466/',
+  './workdir/Study0028/0468/',
+  './workdir/Study0028/0471/',
+  './workdir/Study0026/0447/',
+  './workdir/Study0026/0457/',
+  './workdir/Study0026/0455/',
+  './workdir/Study0026/0453/',
+  './workdir/Study0026/0450/',
+  './workdir/Study0026/0451/',
+  './workdir/Study0022/0418/',
+  './workdir/Study0022/0417/',
+  './workdir/Study0021/0409/',
+  './workdir/Study0021/0414/',
+  './workdir/Study0021/0415/',
+  ]
+  
+  ## resultfileList = [
+  ## './workdir/Study0035/0530/',
+  ## ]
+  
+  def DiceTxtFileParse(DiceInputFilename):
+    # (1) split on ':' (2)  filter lists > 1 (3) convert to dictionary
+    c3doutput = dict(filter( lambda x: len(x) > 1,[line.strip().split(':') for line in open(DiceInputFilename) ] ))
+    return float(c3doutput['Dice similarity coefficient'])
+  
+  texHandle  = open('datasummary.tex' , 'w') 
+  fileHandle = open('datasummary.txt' , 'w') 
+  # write header
+  fileHandle.write("iddata,mu_eff,alpha,robin,obj\n")
+  # loop over files and extract optimal value
+  opttype = 'bestfit'
+  for filenamebase in resultfileList:
+    # get latex command
+    config = ConfigParser.SafeConfigParser({})
+    inisetupfile = '%s/opt/setup.ini' % (filenamebase)
+    config.read(inisetupfile)
+  
+    filename = '%s/opt/dakota_q_newton_%s.in.tabular.dat' % (filenamebase,opttype)
+    dataid = int(filename.split('/')[3])
+    # count the file lines
+    filelines = len(open(filename).readlines(  ))
+    if(filelines > 1):
+      # the objective function value should be the last row
+      obj_fn_data = numpy.loadtxt(filename,skiprows=1,usecols=(0,-1))
+      if(filelines > 2):
+        idmin = int(obj_fn_data[numpy.argmin(obj_fn_data,0)[1]][0])
+        minobjval =  numpy.min(obj_fn_data,0)[1]
+      else:
+        print "single entry ??", mu_eff, obj_fn
+        idmin = 1
+        minobjval =  obj_fn 
+      print idmin,minobjval 
+      dakotafilename = '%s/opt/optpp_pds.%s.in.%d' % (filenamebase,opttype,idmin)
+      opt_fem_params = ParseInput(dakotafilename,False)
+      simvariable = opt_fem_params['cv']     
+      #dataarray = numpy.loadtxt(filename,skiprows=1,usecols=(0,1,2,3,4,6)
+      fileHandle.write("%05d,%s,%s,%s,%s,%12.5e\n" %( dataid, 
+                                                                      simvariable['mu_eff_healthy'],
+                                                                      simvariable['alpha_healthy'],
+                                                                      simvariable['robin_coeff'],
+                                                                      simvariable['gamma_healthy'],
+                                                                      minobjval))
+      # FIXME
+      runcmd = "vglrun python ./brainsearch.py --param_file  %s/opt/optpp_pds.%s.in.%d %s/opt/optpp_pds.%s.out.%d --vis_out" % (filenamebase,opttype,idmin,filenamebase,opttype,idmin)
+      print runcmd
+      #os.system( runcmd )
+  
+      # get arrhenius dice value
+      heattimeinterval               = eval(config.get('mrti','heating')  )
+      SEMDataDirectory               = outputDirectory % int(filenamebase.split('/')[-2]) 
+      dicefilename = "%s/dice.%s.%04d.txt" % (SEMDataDirectory,opttype,heattimeinterval[1])
+      print dicefilename 
+      dicevalue = DiceTxtFileParse(dicefilename)
+  
+      # format latex ouput
+      outputformat                   = config.get('latex','heating')
+      texFormat = outputformat % (minobjval,dicevalue)
+      print texFormat 
+      texHandle.write("%s\n" %(texFormat))
+    else:
+      print "############# %s error ???" % filename
+
 
 # run planning solver w/ default options from ini file
 elif (options.config_ini != None):
