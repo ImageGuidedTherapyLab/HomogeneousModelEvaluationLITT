bc6225fefac6cbaa80f696d2a95efd0ed907b9b3
Samuel Fahrenholtz <samuel.fahrenholtz@gmail.com>
2014-10-15 17:39:22 -0500

    Dropped the Study with 2 slices: Study0023/0428, I think.

--- brainsearch.py
+++ brainsearch.py
@@ -1447,9 +1447,9 @@ if (options.param_file != None):
 # find the best point for each run
 elif (options.accum_history ):
   resultfileList = [
-  './workdir/Study0035/0530/',
+  #'./workdir/Study0035/0530/',
   #'./workdir/Study0023/0433/',
-  './workdir/Study0023/0428/',
+  #'./workdir/Study0023/0428/',
   ##'./workdir/Study0023/0425/',
   './workdir/Study0030/0495/',
   './workdir/Study0030/0497/',
@@ -1492,7 +1492,7 @@ elif (options.accum_history ):
   ##'./workdir/Study0006/0183/',
   ]
   resultfileList = [
-  './workdir/Study0023/0428/',
+  #'./workdir/Study0023/0428/',
   './workdir/Study0030/0495/',
   './workdir/Study0030/0497/',
   './workdir/Study0030/0488/',
