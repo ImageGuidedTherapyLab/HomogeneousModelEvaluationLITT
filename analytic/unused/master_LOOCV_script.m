5f56e54896af85d15df8e81c33028e6c4cfc4eee
Samuel Fahrenholtz <samuel.fahrenholtz@gmail.com>
2014-10-22 10:44:39 -0500

    I added a sorting function that ensures the DSC and mu thresholds are arranged in ascending order, which is critical.

--- analytic/unused/master_LOOCV_script.m
+++ analytic/unused/master_LOOCV_script.m
@@ -4,7 +4,9 @@ clear
 opttype = 'bestfit50';          % Name the opttype
 datafilename = 'datasummaryL2_10sourceNewton50.txt';  % Name the datasummary file
 DSC_thresholds = [0.3 0.5 0.7];
+DSC_thresholds = sort(DSC_thresholds);
 mu_thresholds = [ 150 200];  % Intervals: 0 to 100, 100 to 150, 150 to 200, 200
+mu_thresholds = sort(mu_thresholds);
 % and above
 % DSC_thresholds = [0.3 0.7];   % At least one DSC threshold is required
 % mu_thresholds = [175];   % Zero or more mu_threholds is possible. 
