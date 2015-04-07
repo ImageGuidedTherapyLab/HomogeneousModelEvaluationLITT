60b268dc7b3ee6de68cf4afcc209f29a51c59dec
Samuel Fahrenholtz <samuel.fahrenholtz@gmail.com>
2015-04-07 15:09:52 -0500

    Added scripts to run naive case

--- analytic/master_naive_script.m
+++ analytic/master_naive_script.m
@@ -10,6 +10,6 @@ mu_thresholds = [];  % Intervals: 0 to 100, 100 to 150, 150 to 200, 200
 mu_thresholds = sort(mu_thresholds);
 
 
-[opt, LOOCV, fig_labels] = master_LOOCV ( opttype, datafilename, DSC_thresholds, mu_thresholds);
+[opt, LOOCV, fig_labels] = master_naive ( opttype, datafilename, DSC_thresholds, mu_thresholds);
 
 survival_plot (LOOCV.dice.values, opt.dice.all, fig_labels, LOOCV.run1, LOOCV.run2);
